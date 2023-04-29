43e318ba0bacd0ac23c12846b738cc32219af6f9
jakub-musial <132155449+jakub-musial@users.noreply.github.com>
2023-04-29 18:39:07 +0200

    Add files via upload

---
+++ C++/Zad_2 (Konstruktor Kopiujacy)/Zad_2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+class Prostokat
+{
+	private:
+
+	float dlugosc;
+	float szerokosc;
+
+	public:
+
+	Prostokat(float d, float sz)
+	{
+		dlugosc = d;
+		szerokosc = sz;
+	}
+
+	Prostokat(const Prostokat &prostokat2)
+	{
+		dlugosc = prostokat2.dlugosc;
+		szerokosc = prostokat2.szerokosc;
+	}
+
+	float pole()
+	{
+		return dlugosc * szerokosc;
+	}
+
+	float obwod()
+	{
+		return 2 * dlugosc + 2 * szerokosc;
+	}
+
+	void set_dlugosc(float d)
+	{
+		dlugosc = d;
+	}
+
+	void set_szerokosc(float sz)
+	{
+		szerokosc = sz;
+	}
+};
+
+int main()
+{
+	cout << "PROSTOKAT" << endl << endl;
+
+	Prostokat prostokat1(4, 5);
+	cout << "Pole: " << prostokat1.pole() << endl;
+	cout << "Obwod: " << prostokat1.obwod() << endl << endl;
+
+	cout << "---------------------------------" << endl << endl;
+	cout << "KOPIA PROSTKATA" << endl << endl;
+
+	Prostokat prostokat2(prostokat1);
+	cout << "Pole: " << prostokat2.pole() << endl;
+	cout << "Obwod: " << prostokat2.obwod() << endl << endl;
+
+	prostokat2.set_dlugosc(10);
+	prostokat2.set_szerokosc(20);
+	cout << "Pole: " << prostokat2.pole() << endl;
+	cout << "Obwod: " << prostokat2.obwod() << endl;
+}
---
+++ C++/Zad_3 (Przyjaźń)/Zad_3.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Macierz
+{
+private:
+    static const int rozmiar = 3;
+    float tablica[rozmiar][rozmiar];
+
+public:
+    static const int a = 4;
+
+    Macierz()
+    {
+        for (int w = 0; w < rozmiar; w++)
+            for (int k = 0; k < rozmiar; k++)
+                tablica[w][k] = 0;
+    }
+    void wczytajDane()
+    {
+        for (int w = 0; w < rozmiar; w++)
+            for (int k = 0; k < rozmiar; k++)
+                cin >> tablica[w][k];
+    }
+    void wypisz()
+    {
+        for (int w = 0; w < rozmiar; w++)
+        {
+            cout << endl;
+            for (int k = 0; k < 3; k++)
+                cout << tablica[w][k] << " ";
+        }
+
+    }
+    friend Macierz* dodajMacierze(Macierz* macierz1, Macierz* macierz2);
+};
+
+
+Macierz* dodajMacierze(Macierz* macierz1, Macierz* macierz2)
+{
+    Macierz* macierz_wynik = new Macierz;
+
+    for (int w = 0; w < Macierz::rozmiar; w++)
+        for (int k = 0; k < Macierz::rozmiar; k++)
+            macierz_wynik->tablica[w][k] = macierz1->tablica[w][k] + macierz2->tablica[w][k];
+    
+    return macierz_wynik;
+}
+
+int main()
+{
+    Macierz* macierz_1 = new Macierz;
+    Macierz* macierz_2 = new Macierz;
+
+    cout << "Wypelnij macierz nr 1: " << endl;
+    macierz_1->wczytajDane();
+    macierz_1->wypisz();
+
+    cout << endl << endl;
+
+    cout << "Wypelnij macierz nr 2: " << endl;
+    macierz_2->wczytajDane();
+    macierz_2->wypisz();
+
+    cout << endl << endl;
+
+
+    cout << "Wynik dodawania macierzy 1 i 2:" << endl << endl;
+    Macierz* macierz_3 = dodajMacierze(macierz_1, macierz_2);
+    macierz_3->wypisz();
+
+    return 0;
+}
